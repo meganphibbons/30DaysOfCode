afbbe286b8c35393ae43bad7af69bfba97f814fd
Phibbons, Megan <megan.phibbons@fmr.com>
2018-06-07 14:06:25 -0400

    Day 1 attempt 3

--- src/Day1.java
+++ src/Day1.java
@@ -19,6 +19,7 @@ public class Day1 {
         /* Read and save an integer, double, and String to your variables.*/
         i2 = scan.nextInt();
         d2 = scan.nextDouble();
+        scan.nextLine();
         s2 = scan.nextLine();
         // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
         
