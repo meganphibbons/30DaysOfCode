ea6a8c942e314f6b1c5b8382f0fd4b4bcbe5c395
Phibbons, Megan <megan.phibbons@fmr.com>
2018-06-11 16:14:09 -0400

    Day 3 attempt 2

--- src/Day3.java
+++ src/Day3.java
@@ -12,10 +12,14 @@ public class Day3 {
     public static void main(String[] args) {
         int N = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-        if(N > 1 && N < 100) {
-        	System.out.println("Not Weird");
-        } else {
+        if(N % 2 != 0) {
         	System.out.println("Weird");
+        } else if(N <= 5 && N % 2 == 0) {
+        	System.out.println("Not Weird");
+        } else if(N <= 20 && N % 2 == 0) {
+        	System.out.println("Weird");
+        } else {
+        	System.out.println("Not Weird");
         }
         scanner.close();
     }
