7eb7d422e0bc26329dc7115c32dc0af074e6343d
Phibbons, Megan <megan.phibbons@fmr.com>
2018-06-11 16:26:59 -0400

    Day 5 attempt 1

---
+++ src/Day5.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.concurrent.*;
+
+public class Day5 {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        for(int i = 1; i <= 10; i++) {
+        	int prod = n * i;
+        	System.out.println(n + " x " + i + " = " + prod);
+        }
+        scanner.close();
+    }
+}
