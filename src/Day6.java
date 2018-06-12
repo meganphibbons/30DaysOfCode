815542baf29c9c182dd6a84f31a06cbe572105fb
Phibbons, Megan <megan.phibbons@fmr.com>
2018-06-12 14:22:45 -0400

    day 6 attempt 1

---
+++ src/Day6.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Day6 {
+
+    public static void main(String[] args) {
+    	Scanner scan = new Scanner(System.in);
+    	int numInputs = scan.nextInt();
+    	for(int i = 0; i < numInputs; i++) {
+    		String s = scan.next();
+    		String even = "";
+    		String odd = "";
+    		for(int j = 0; j < s.length(); j++) {
+    			if(j % 2 == 0) {
+    				even += s.charAt(j);
+    			} else {
+    				odd += s.charAt(j);
+    			}
+    		}
+    		System.out.println(even + " " + odd);
+    	}
+    	scan.close();
+    }
+}
+
