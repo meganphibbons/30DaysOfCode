5cc897ac9c5a8684ca2f1f233f76f6fb69d52448
Phibbons, Megan <megan.phibbons@fmr.com>
2018-06-18 09:52:45 -0400

    day 11 final attempt

--- src/Day11.java
+++ src/Day11.java
@@ -28,8 +28,8 @@ public class Day11 {
             }
         }
         
-        int maxSum = 0;
-        int currSum = Integer.MIN_VALUE;
+        int maxSum = Integer.MIN_VALUE;
+        int currSum = 0;
         for(int r = 0; r < 4; r++) {
         	for(int c = 0; c < 4; c++) {
         		currSum = arr[r][c] + arr[r][c + 1] + arr[r][c + 2] + arr[r + 1][c + 1] + arr[r + 2][c] + arr[r + 2][c + 1] + arr[r + 2][c + 2];
