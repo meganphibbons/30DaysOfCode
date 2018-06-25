a13a3c12fdd37417e128810ddc51b4086a710fad
Phibbons, Megan <megan.phibbons@fmr.com>
2018-06-25 10:03:07 -0400

    day 17 attempt 1

--- src/Day16.java
+++ src/Day16.java
@@ -1,14 +1,11 @@
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 public class Day16 {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String S = in.next();
+        in.close();
         try {
         	System.out.println(Integer.parseInt(S));
         } catch (NumberFormatException e) {
---
+++ src/Day17.java
@@ -0,0 +1,9 @@
+public class Day17 {
+    public int power(int n, int p) {
+        if(n < 0 || p < 0) {
+            throw new IllegalArgumentException("n and p should be non-negative");
+        } else {
+            return (int) Math.pow(n, p);
+        }
+    }
+}
