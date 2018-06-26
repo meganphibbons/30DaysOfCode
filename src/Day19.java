5a10cb2b418edc58c78390aeb53b2da0457afcb0
Phibbons, Megan <megan.phibbons@fmr.com>
2018-06-26 08:13:12 -0400

    fixed for loop bounds

--- src/Day19.java
+++ src/Day19.java
@@ -1,7 +1,7 @@
 class Day19 implements Day19Support {
     public int divisorSum(int n) {
         int sum = 0;
-        for(int i = 1; i < n; i++) {
+        for(int i = 1; i <= n; i++) {
             if(n % i == 0) {
                 sum += i;
             }
